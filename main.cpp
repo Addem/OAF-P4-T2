2f038c139f3033066d2a4794a4048f63df8edf7f
Addem <adamqah1@gmail.com>
2016-11-28 19:29:20 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include "library/linsearch.hpp"
-#include "library/counting.hpp"
 #include "library/summation.hpp"
 #include "library/seqinfileenumerator.hpp"
 #include <fstream>
 
 struct Pair {
   std::string name;
+  int payment;
   int sum;
   friend std::ostream& operator<<(std::ostream&, const &Pair);
   friend std::istream& operator>>(std::istream&, Pair&);
 };
 
 std::istream& operator>>(std::istream &f, Pair &pa) {
-  f >> pa.name >> pa.sum;
+  f >> pa.name >> pa.payment;
   return f;
 }
 
@@ -24,7 +24,7 @@ std::ostream& operator<<(std::ostream& o, const Pair &pa) {
 
 class MyLinSearch : public LinSearch<Pair> {
   public:
-   bool cond(const Pair &p) const { return p.sum<-100000; }
+   bool cond(const Pair &p) const { return p.sum < -100000; }
 };
 
 class PairEnumerator : public Enumerator<Pair> {
@@ -33,7 +33,7 @@ class PairEnumerator : public Enumerator<Pair> {
    Pair _current;
    bool _end;
   public:
-   PairEnumerator(const Pair& str) { _f = new SeqInFileEnumerator<Pair>(str); }
+   PairEnumerator(const std::string &file_name_) { _f = new SeqInFileEnumerator<Pair>(file_name_); }
    ~PairEnumerator(){ delete _f; }
    void first() { _f->first(); next(); }
    void next();
@@ -41,28 +41,31 @@ class PairEnumerator : public Enumerator<Pair> {
    Pair current() const { return _current; }
 };
 
-/*class PairSum : public Summation<Pair> {
+class PairSum : public Summation<Pair, int> {
+  private:
+   const std::string name_;
+   int current_sum_;
   protected:
-   string _name;
-   void first(){}
-   void add 
-   bool whileCond(const Pair& e) const { return e.name == _name; }
+   void first() {}
+   void init() { current_sum_ = 0; }
+   bool whileCond(const Pair& e) const { return e.name == name_; }
+   void add(const Pair &e ) { current_sum_ += e.payment; *_result = current_sum_; }
   public:
-   PairSum(const Pair &str):
-   Summation<Pair>(), _name(str){}
-};*/
-/*
+   PairSum( std::string &name ):
+   Summation<Pair, int>(), name_(name) {}
+};
+
 void PairEnumerator::next() {
   if((_end = _f->end())) return;
-  _current.pname = _f->current();
-  PairSum pr(_current.pname); pr.addEnumerator(_f);
+  _current.name = _f->current().name;
+  PairSum pr(_current.name); pr.addEnumerator(_f);
   pr.run();
   _current.sum = pr.result();
-} */
+}
 
 int main() {
   MyLinSearch pr;
-  //PairEnumerator t("test.txt"); pr.addEnumerator(&t);
+  PairEnumerator t("test.txt"); pr.addEnumerator(&t);
   pr.run();
   
   if (pr.found()) std::cout << "Eredmeny: Nem" << std::endl;
